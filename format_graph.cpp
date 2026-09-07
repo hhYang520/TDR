377819f33662d4fd1c7b36cc7468b2cb13ce40d0
木心 <50612078+hhYang520@users.noreply.github.com>
2026-09-07 22:38:56 +0800

    Update format_graph.cpp

--- format_graph.cpp
+++ format_graph.cpp
@@ -1,7 +1,3 @@
-//
-// Created by Yang on 2025/11/19.
-//
-
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -28,65 +24,50 @@ int main(int argc, char *argv[]) {
   string line;
   map<int, int> vertices;
   map<string, int> labels;
-  vector<map<int, set<int>>> edges; //标记边
+  vector<map<int, set<int>>> edges;
   int vertices_num = 0, label_num = 0, labeled_edges_num = 0;
   while (getline(file, line)) {
     istringstream iss(line);
     std::vector<std::string> tokens;
     std::string token;
 
-    // 以空格划分每一行，并存储到 tokens 向量中
     while (iss >> token) {
       tokens.push_back(token);
     }
 
     int u, v;
     string l;
-    //如果是空行，跳过
     if (tokens.empty())
       continue;
     try {
-      // 尝试读取三个变量
       u = stoi(tokens[0]);
       v = stoi(tokens[1]);
       l = tokens[2];
     } catch (const std::exception &e) {
-      // 如果变量类型有错误，则跳过该行
       continue;
     }
 
-    //去除自环边
     if (u == v)
       continue;
 
-    //顶点检查
     auto it1 = vertices.insert({u, vertices_num});
-    //插入成功，说明顶点不存在
     if (it1.second) {
       vertices_num++;
-      // 创建一个空的 map<int, set<int>>
       map<int, set<int>> emptyMap;
-      // 将空的 map 推入到 vector 中
       edges.push_back(emptyMap);
     }
     it1 = vertices.insert({v, vertices_num});
-    //插入成功，说明顶点不存在
     if (it1.second) {
       vertices_num++;
-      // 创建一个空的 map<int, set<int>>
       map<int, set<int>> emptyMap;
-      // 将空的 map 推入到 vector 中
       edges.push_back(emptyMap);
     }
 
-    //标签检查
     auto it2 = labels.insert({l, label_num});
-    //若插入成功，说明标签不存在
     if (it2.second) {
       label_num++;
     }
 
-    //插入边
     auto it3 = edges[vertices[u]][vertices[v]].insert(labels[l]);
     if (it3.second) {
       labeled_edges_num++;
@@ -99,7 +80,6 @@ int main(int argc, char *argv[]) {
   file << "vertices: " << vertices.size() << " topologyEdges: " << edges.size()
        << " labeledEdges: " << labeled_edges_num << " labels: " << labels.size()
        << endl;
-  // The number of static edges, temporal edges and vertices.
   int uid = 0;
   for (auto &edge : edges) {
     for (const auto &e : edge) {
